843ff64c5fd9ae56796525de9737f3c25e371795
Asif77860 <officialasif7860@gmail.com>
2025-07-23 18:17:51 +0530

    Added new Files

--- taskbuddy/src/components/TaskForm.jsx
+++ taskbuddy/src/components/TaskForm.jsx
@@ -3,13 +3,30 @@ import React, { useState } from 'react'
 export default function 
 () {
     const [task,setTask] = useState('');
+    const [priority,setPriority] = useState('Medium');
+    const [category,setCategory] = useState('General');
+
   return (
-    <form>
-        <input type='text'
+    <form class='task-form'>
+       <div id='btn'>
+         <input type='text'
         placeholder='Enter Your Task'
         required value={task}
         onChange={(e)=>setTask(e.target.value)}/>
-        <h1>{task}</h1>
+        <span><button>Add Tasks</button></span>
+        </div>
+    <div class='btns'>
+        <select value={priority} onChange={(e)=>setPriority(e.target.value)}>
+            <option>Medium</option> 
+            <option>Low</option>
+            <option>High</option>
+        </select>
+        <select value={category} onChange={(e)=>setCategory(e.target.value)}>
+            <option>General</option> 
+            <option>Work</option>
+            <option>Personal</option>
+        </select>
+    </div>
     </form>
   )
 }
